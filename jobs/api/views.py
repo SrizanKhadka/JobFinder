b3e14abc20a9c4dce706fc776a1876a34e05c9ce
srijank <srijan@pathway.com.np>
2024-03-19 15:35:22 +0545

    created a IsJobCreatorOrEmployee permission.

--- jobs/api/views.py
+++ jobs/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from jobs.api.serializer import JobIndustrySerializer, JobSerializer
 from jobs.models import JobModel, JobIndustry
 from rest_framework import permissions
-from jobs.api.permissons import IsJobCreator, IsEmployer
+from jobs.api.permissons import IsJobCreatorOrEmployer
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 from rest_framework import status
@@ -11,19 +11,19 @@ from rest_framework import status
 class CreateJobsView(viewsets.ModelViewSet):
     serializer_class = JobSerializer
     queryset = JobModel.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsEmployer]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsJobCreatorOrEmployer]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
         
     def update(self, request, *args, **kwargs):
         print(f"PRIMARY KEY = {kwargs['pk']}")
-        self.permission_classes = [IsJobCreator]
+        self.permission_classes = [IsJobCreatorOrEmployer]
         response = super().update(request, *args, **kwargs)
         return Response({"data:":response.data, "message":"Job updated Successfully!"})
     
     def destroy(self, request, *args, **kwargs):
-        self.permission_classes = [IsJobCreator]
+        self.permission_classes = [IsJobCreatorOrEmployer]
         super().destroy(request, *args, **kwargs)
         return Response({"details":"Job deleted successfully"})
 
